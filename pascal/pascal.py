93dd702eec0c75c6e2cd12dcf33f205516288206
Sergio Cano <canosergio90@gmail.com>
2022-08-29 09:38:44 +0200

    refactors in pascal
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- pascal/pascal.py
+++ pascal/pascal.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-def pascal_value(level, column):
+def pascal(level, column):
     if level == 1:
         return 1
     elif column == 0:
-        return pascal_value(level - 1, column)
+        return pascal(level - 1, column)
     elif column == level - 1:
-        return pascal_value(level - 1, column - 1)
+        return pascal(level - 1, column - 1)
     else:
-        return pascal_value(level - 1, column) + pascal_value(level - 1, column - 1)
+        return pascal(level - 1, column) + pascal(level - 1, column - 1)
+
+def show_pascal_tree():
+    pass
--- pascal/pascal_test.py
+++ pascal/pascal_test.py
@@ -1,22 +1,21 @@
 import unittest
-from pascal import pascal_value
+from pascal import pascal
 
 class PascalTest(unittest.TestCase):
 
     # The following tests are testing the "get_roman_numeral()" method
     def test_first_row(self):
-        result = pascal_value(1, 0)
+        result = pascal(1, 0)
         self.assertEqual(1, result)
 
     def test_second_row(self):
-        result = pascal_value(2, 0)
+        result = pascal(2, 0)
         self.assertEqual(1, result)
 
     def test_second_row_intermediate_element(self):
-        result = pascal_value(2, 1)
+        result = pascal(2, 1)
         self.assertEqual(1, result)
     
-    # fixme
     def test_n_level_element(self):
-        result = pascal_value(5, 2)
+        result = pascal(5, 2)
         self.assertEqual(6, result)
