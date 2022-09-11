0b56ed03d9a52ec1cac980d939a31262a7c8d36e
Sergio Cano <canosergio90@gmail.com>
2022-09-11 15:23:37 +0200

    partition equal subset sum | solution using dynamic programming
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- partition_equal_subset_sum/partition_equal_subset_sum.py
+++ partition_equal_subset_sum/partition_equal_subset_sum.py
@@ -1,13 +1,26 @@
 def partition_equal_subset_sum(nums):
+    """solution using dynamic programming"""
     total = sum(nums)
     if total % 2 != 0:
         return False
-    return canPartition(nums, 0, 0, total)
+    return canPartition(nums, 0, 0, total, {})
 
 
-def canPartition(nums, index, sum, total):
+def canPartition(nums, index, sum, total, state):
+    # using dict/hash map to avoid recalculating already computed values
+    current = str(index) + str(sum)
+    if (state.get(current) != None):
+        return state.get(current)
+
     if (sum * 2 == total):
         return True
+
     if (sum * 2 > total or index >= len(nums)):
         return False
-    return canPartition(nums, index + 1, sum, total) or canPartition(nums, index + 1, sum + nums[index], total)
+
+    foundPartition = canPartition(
+        nums, index + 1, sum, total, state) or canPartition(nums, index + 1, sum + nums[index], total, state)
+
+    state[current] = foundPartition
+
+    return foundPartition
